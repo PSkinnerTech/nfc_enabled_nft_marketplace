2def13fca076e056f176e3d0482c4bca636be4bd
Patrick Skinner <78289253+PSkinnerTech@users.noreply.github.com>
2023-04-23 00:08:45 -0500

    still solving circular dependancy issue using Vercel

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,4 +1,4 @@
-const Button = ({ btnName, classStyles, handleClick }) => (
+export const Button = ({ btnName, classStyles, handleClick }) => (
   <button
     type="button"
     className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`}
@@ -7,5 +7,3 @@ const Button = ({ btnName, classStyles, handleClick }) => (
     {btnName}
   </button>
 );
-
-export default Button;
