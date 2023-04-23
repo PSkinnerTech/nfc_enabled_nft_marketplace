6dc81aa1db906f8163963e8706b4b3fd0bc86c0b
Patrick Skinner <78289253+PSkinnerTech@users.noreply.github.com>
2023-04-23 00:17:58 -0500

    removed routed page for early demo deployment on Vercel

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ const Home = () => (
   <div className="flex justify-center sm:px-4 p-12">
     <div className="w-full minmd:w-4/5">
       <Banner
-        name="Discover, collect, and sell extraordinary NFTs"
+        name="Discover, collect, and sell extraordinary physical NFTs"
         childStyles="md:text-4xl sm:text-2xl sx:text-xl text-left"
         parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
       />
