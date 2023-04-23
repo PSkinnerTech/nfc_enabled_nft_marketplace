d5495ecd26cda7883f69c43ab1e8f11de0938a0c
Patrick Skinner <78289253+PSkinnerTech@users.noreply.github.com>
2023-04-23 00:12:54 -0500

    solved the circular dependancy error in the footer, now salving for the Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import images from '../assets';
-import { Button } from './';
+import { Button } from './Button';
 
 // May need to add 'setActive' to MenuItems props later
 const MenuItems = ({ isMobile, active }) => {
